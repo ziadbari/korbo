f7fe01c77400bc4ed38755df8399ce2852cb73ac
UseR <2024100000367@seu.edu.bd>
2025-11-07 00:20:19 +0600

    initial commit

---
+++ src/product.java
@@ -0,0 +1,36 @@
+public class product {
+
+    public static void main(String[]args)
+    {
+        System.out.println("id:101");
+
+        System.out.println("title:iphone 15");
+        System.out.println("price:1895 euros");
+
+        System.out.println("description:perfect product with best image quality");
+        System.out.println("category:phone");
+        System.out.print("1\t 2");
+        System.out.println("\"java programming\"");
+        System.out.println("01712793899 \nkante");
+        System.out.println("3 \t 6");
+        System.out.println("4 \t 7");
+        System.out.println("Fuck you so much");
+        System.out.println("konami")   ;
+        System.out.println("34\t 67");
+        System.out.println("45\t 90");
+        System.out.println("bogura -6");
+        System.out.println("tareq Rahaman");
+        System.out.println("even numbers are not eligible") ;
+        System.out.println("age=12");
+        System.out.println("ziad");
+        System.out.println("id num");
+        System.out.println("\'bari\'");
+        //public=access modifier;class=keyword; public static void main(string[]args); single comment
+        /*
+        my name is ziad ul bari.I am a junior programmer.in this semester i take introduction to java course and my faculty is ashraful i8slam paran sir.
+         */
+
+
+    }
+}
+
